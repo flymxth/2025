1314b0a520a3b7094ee883e3f44dc5d0cf007ad7
flymxth <cugl070103hneszsa0@cbtis217.edu.mx>
2025-04-27 11:43:45 -0600

    DUENDE

---
+++ DUENDE/Funciones JS/script.js
@@ -0,0 +1,17 @@
+var menu = document.querySelector('#menu-icon');
+var navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay: 200, origin: 'top'});
+sr.reveal('.hero-img', {delay: 450, origin: 'top'});
